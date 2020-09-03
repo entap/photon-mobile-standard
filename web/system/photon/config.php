cf372af2d8fbccd19c664d5d7ccde7cd5ce7d485
ii masamitsu <ii@entap.co.jp>
2020-09-04 06:28:45 +0900

    docker-composeで起動できるようにした

--- web/system/photon/config.php
+++ web/system/photon/config.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
 
 // データベースの設定
-config('db_hostname', 'localhost');
+config('db_hostname', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
 config('db_username', 'photon-mobile-standard');
 config('db_password', '2tv8YdBAfuSL');
 config('db_database', 'photon-mobile-standard');
